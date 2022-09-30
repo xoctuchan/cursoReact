7f13e773b08401c32c673519d6890e9fff828600
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-09-30 15:24:00 -0600

    Async-await  operadorTernario

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,27 @@
+// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
+// getImagenPromesa().then( console.log );
+
+const getImagen = async() => {
+
+    try {
+
+        const apiKey = '27cyNr4DmZzKWTq37KwKHwezoEQigFfr';
+        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const { data } = await resp.json(); 
+
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+
+    } catch (error) {
+        // manejo del error
+        console.error(error)
+    }
+    
+    
+    
+}
+
+ getImagen();
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,12 +1,5 @@
-const apiKey = '27cyNr4DmZzKWTq37KwKHwezoEQigFfr';
-const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+const getImagen = ()=>{
+    return 'url';
+}
 
-peticion
-.then((resp)=>resp.json())
-.then(({data}) =>{
-    const {url} = data.images.original
-    const img= document.createElement('img');
-    img.src=url;
-    document.body.append(img);
-})
-.catch(console.warn)
+console.log(getImagen());
