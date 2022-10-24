82d176237353afea15cf398fb06dad4096d6256a
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-10-24 10:56:25 -0600

    Sección 14 HeroesApp

---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { HeroesRoutes } from '../heroes'
+
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path='login' element={ <LoginPage/> }/>
+                <Route path='/*' element={<HeroesRoutes/> }/>
+            </Routes>
+        </>
+    )
+}
