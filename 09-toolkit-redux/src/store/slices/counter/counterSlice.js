4cb8d3e6becae7d4149f7a919eb19e96df71e6eb
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-10-27 14:02:43 -0600

    Sección 18 - Redux - Que es y conceptos

---
+++ 09-toolkit-redux/src/store/slices/counter/counterSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState:{
+        counter: 10
+    },
+    reducers: {
+        increment: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+            state.counter += 1
+        },
+        decrement: (state) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+                state.counter -= 1
+        },
+        incrementBy: (state, action) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+                state.counter += action.payload
+        },
+
+    },
+})
+
+export const { increment, decrement, incrementBy } = counterSlice.actions;
